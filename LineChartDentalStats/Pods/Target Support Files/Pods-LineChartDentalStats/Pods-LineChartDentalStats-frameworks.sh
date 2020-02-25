0b5fe69c12732a5ea98e1182638501bc16280dc2
Raymond Chen <ahrchen@gmail.com>
2020-02-25 12:48:00 -0800

    Add Chart Third Party Dependency

--- LineChartDentalStats/Pods/Target Support Files/Pods-LineChartDentalStats/Pods-LineChartDentalStats-frameworks.sh
+++ LineChartDentalStats/Pods/Target Support Files/Pods-LineChartDentalStats/Pods-LineChartDentalStats-frameworks.sh
@@ -162,10 +162,12 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Charts/Charts.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SteviaLayout/Stevia.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Charts/Charts.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SteviaLayout/Stevia.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
